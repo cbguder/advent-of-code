c798ea59e9a365b57b5f4f88a5a61161668e74f9
Can Berk Güder <cbg@cbg.me>
2021-12-05 22:51:39 -0800

    Day 6

---
+++ 06/part2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+from collections import defaultdict
+
+def main():
+    fish = defaultdict(int)
+
+    with open('input') as f:
+        lines = f.readlines()
+    
+    for f in map(int, lines[0].split(',')):
+        fish[f] += 1
+    
+    for i in range(256):
+        newfish = defaultdict(int)
+
+        for k, v in fish.items():
+            if k == 0:
+                newfish[6] += v
+                newfish[8] += v
+            else:
+                newfish[k-1] += v
+        
+        fish = newfish
+    
+    print(sum(fish.values()))
+
+
+if __name__ == '__main__':
+    main()
