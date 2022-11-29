7aba9862761bffb1f103d4f9aa293ad1dbcc4fd0
Can Berk Güder <cbg@cbg.me>
2022-11-28 16:50:54 -0800

    Add template

---
+++ template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def main():
+    with open("input") as f:
+        for line in f:
+            line = line.strip()
+            pass
+
+
+if __name__ == "__main__":
+    main()
