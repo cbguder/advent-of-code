06cb049c3af36ea06b1749085fb24f8d19932d20
Can Berk Güder <cbg@cbg.me>
2022-12-03 21:13:25 -0800

    Day 4

---
+++ 2022/04/part2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def main():
+    count = 0
+
+    with open("input") as f:
+        for line in f:
+            line = line.strip()
+            pair = tuple(map(lambda x: tuple(map(int, x.split("-"))), line.split(",")))
+            aset = set(range(pair[0][0], pair[0][1] + 1))
+            bset = set(range(pair[1][0], pair[1][1] + 1))
+            if len(aset & bset) > 0:
+                count += 1
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
