22bc0ab8178ff6e8d27a67d6dc209c47472ca941
Can Berk Güder <cbg@cbg.me>
2022-12-04 21:15:27 -0800

    Day 5

---
+++ 2022/05/part1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+import re
+
+
+MOVE_RE = r"move (\d+) from (\d+) to (\d+)"
+
+
+def main():
+    stacks = []
+
+    for i in range(9):
+        stacks.append([])
+
+    with open("input") as f:
+        for line in f:
+            if line.startswith("["):
+                for i in range(9):
+                    crate = line[1 + i * 4]
+                    if crate != " ":
+                        stacks[i] = [crate] + stacks[i]
+            else:
+                match = re.match(MOVE_RE, line)
+                if match is not None:
+                    count = int(match.group(1))
+                    from_crate = int(match.group(2))
+                    to_crate = int(match.group(3))
+
+                    for i in range(count):
+                        box = stacks[from_crate-1].pop()
+                        stacks[to_crate-1].append(box)
+
+    tops = [stack[-1] for stack in stacks]
+    print("".join(tops))
+
+
+if __name__ == "__main__":
+    main()
