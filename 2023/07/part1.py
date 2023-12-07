81c08129f1d6dc7e8c72d5626e30197e933a604f
Can Berk Güder <cbg@cbg.me>
2023-12-06 21:24:30 -0800

    Day 7

---
+++ 2023/07/part1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+from functools import cmp_to_key
+
+cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+
+def main():
+    hands = []
+
+    with open("input") as f:
+        for line in f:
+            parts = line.strip().split()
+            hand = parts[0]
+            bid = int(parts[1])
+            hands.append((hand, bid))
+
+    hands.sort(key=cmp_to_key(cmp))
+
+    ret = 0
+    for i in range(len(hands)):
+        ret += hands[i][1] * (i + 1)
+    print(ret)
+
+
+def cmp(a, b):
+    a_type = hand_type(a[0])
+    b_type = hand_type(b[0])
+
+    if a_type < b_type:
+        return 1
+    elif a_type > b_type:
+        return -1
+
+    for i in range(5):
+        a_idx = cards.index(a[0][i])
+        b_idx = cards.index(b[0][i])
+        if a_idx < b_idx:
+            return 1
+        elif a_idx > b_idx:
+            return -1
+
+    return 0
+
+
+def hand_type(hand):
+    counts = defaultdict(int)
+    for card in hand:
+        counts[card] += 1
+
+    if len(counts) == 1:
+        return 0  # FIVE_OF_A_KIND
+    elif len(counts) == 2:
+        if 4 in counts.values():
+            return 1  # FOUR_OF_A_KIND
+        else:
+            return 2  # FULL_HOUSE
+    elif len(counts) == 3:
+        if 3 in counts.values():
+            return 3  # THREE_OF_A_KIND
+        else:
+            return 4  # TWO_PAIR
+    elif len(counts) == 4:
+        return 5  # ONE_PAIR
+
+    return 6  # HIGH_CARD
+
+
+if __name__ == "__main__":
+    main()
