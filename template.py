15193e43f10db407e4ac038a61455246ea6075fe
Can Berk Güder <cbg@cbg.me>
2024-12-16 17:33:42 -0800

    Update template

--- template.py
+++ template.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+import pyperclip
+
 
 def main():
     with open("input") as f:
         for line in f:
             line = line.strip()
             pass
+
+    ret = 0
+
+    print(ret)
+    pyperclip.copy(ret)
 
 
 if __name__ == "__main__":
